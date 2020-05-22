c74d7889833b3666f9f09e5ecbc3910b3d9c2f4c
ShinobiWPS <axanderghemov@gmail.com>
2020-05-22 22:38:19 +0200

    Crea l'API di bitstamp per ottenere lo status dell Order

--- sorgenti/piattaforme/bitstamp/bitstampRequests.py
+++ sorgenti/piattaforme/bitstamp/bitstampRequests.py
@@ -88,7 +88,7 @@ def buyORsell(operation:str,xrp:str):
 	# todo- ON SELL ERROR:
 	return r.content
 
-def getPortafoglio():
+def getBalance():
 	"""Ottieni XRP ed EUR disponibili
 
 	Returns:
@@ -124,4 +124,42 @@ def getPortafoglio():
 		data=payload_string
 	)
 	# /balance puo' failare solo per colpa dell'autenticazione, non ha risposte negative
-	return [r.content['xrp_available'], r.content['eur_available']]
+	return r.content
+
+def getOrderStatus(order_id):
+	"""Ottieni XRP ed EUR disponibili
+
+	Returns:
+		list -- array di XRP,EUR disponibili
+	"""
+	payload = {'id':order_id}
+
+	payload_string = urlencode(payload)
+
+	# '' (empty string) in message represents any query parameters or an empty string in case there are none
+	message = 'BITSTAMP ' + api_key + \
+			  'POST' + \
+			  'www.bitstamp.net' + \
+			  '/api/order_status/' + \
+			  '' + \
+			  nonce + \
+			  timestamp + \
+			  'v2' + \
+			  payload_string
+	message = message.encode('utf-8')
+	signature = hmac.new(API_SECRET, msg=message,
+						 digestmod=hashlib.sha256).hexdigest()
+	headers = {
+		'X-Auth': 'BITSTAMP ' + api_key,
+		'X-Auth-Signature': signature,
+		'X-Auth-Nonce': nonce,
+		'X-Auth-Timestamp': timestamp,
+		'X-Auth-Version': 'v2',
+	}
+	r = requests.post(
+		'https://www.bitstamp.net/api/order_status/',
+		headers=headers,
+		data=payload_string
+	)
+
+	return r.content
