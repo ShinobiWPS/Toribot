d22a9a5a9dbeadf0447c20bc169c8a4611650a57
ShinobiWPS <axanderghemov@gmail.com>
2020-06-05 17:21:38 +0200

    Aggiunta ulteriori stili di formattazione(ma sta funzionando?)

--- sorgenti/play.py
+++ sorgenti/play.py
@@ -55,12 +55,13 @@ strategiaSigla = sys.argv[1]
 # no error handling on purpose,
 # we want to crash the bot if a correct strategy name it's not provided
 path = f'strategie.{strategiaSigla}'
-#CANALE='order_book'
+# CANALE='order_book'
 CANALE = 'live_trades'
-strategiaModulo= importlib.import_module(path)
+strategiaModulo = importlib.import_module(path)
 
 ULTIMI_VALORI = []
 NUMERO_ULTIMI_VALORI = 5
+
 
 def avvio():
 	try:
@@ -72,7 +73,7 @@ def avvio():
 		dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 		# pulisco il report prime di scriverci sopra
-		gestoreRapporti.FileWrite(TRADING_REPORT_FILENAME,"")
+		gestoreRapporti.FileWrite(TRADING_REPORT_FILENAME, "")
 
 		if "dev" in argv:
 			portafoglio("cripto", 0)
@@ -82,11 +83,13 @@ def avvio():
 		cripto, soldi = portafoglio()
 		if soldi:
 			print("Inizio con " + str(round(soldi, 2)) + " " + str(MONETA))
-			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,dt_string+" Inizio con " + str(round(soldi, 2)) + " " + str(MONETA))
+			gestoreRapporti.FileAppend(
+				TRADING_REPORT_FILENAME, dt_string+" Inizio con " + str(round(soldi, 2)) + " " + str(MONETA))
 		if cripto:
 			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,
-			    dt_string+" Inizio con " + str(round(cripto, 3)) + " " + str(CRIPTOMONETA)
-			)
+									   dt_string+" Inizio con " +
+									   str(round(cripto, 3)) +
+									   " " + str(CRIPTOMONETA))
 			print("Inizio con " + str(round(cripto, 3)) + " " + str(CRIPTOMONETA))
 		sys.stdout.flush()
 
@@ -97,22 +100,25 @@ def avvio():
 
 		cripto, soldi = portafoglio()
 		if soldi:
-			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,dt_string+" Finisco con " + str(round(soldi, 2)) + " " + str(MONETA))
+			gestoreRapporti.FileAppend(
+				TRADING_REPORT_FILENAME, dt_string+" Finisco con " + str(round(soldi, 2)) + " " + str(MONETA))
 			print("Finisco con " + str(round(soldi, 2)) + " " + str(MONETA))
 		if cripto:
 			ultimo_valore = commercialista()[0]
 			print(
-			    "Finisco con " + str(round(cripto * ultimo_valore, 2)) + " " +
-			    str(MONETA)
+				"Finisco con " + str(round(cripto * ultimo_valore, 2)) + " " +
+				str(MONETA)
 			)
 			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,
-			    dt_string+" Finisco con " + str(round(cripto, 3)) + " " + str(CRIPTOMONETA)
-			)
+									   dt_string+" Finisco con " +
+									   str(round(cripto, 3)) +
+									   " " + str(CRIPTOMONETA)
+									   )
 			#gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,dt_string+" Finisco con " + str(round(cripto * (ultimo_valore if ultimo_valore > valore_acquisto else valore_acquisto), 2)) + " " + str(MONETA))
 			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,
-			    dt_string+" Finisco con " + str(round(cripto * ultimo_valore, 2)) + " " +
-			    str(MONETA)
-			)
+									   dt_string+" Finisco con " + str(round(cripto * ultimo_valore, 2)) + " " +
+									   str(MONETA)
+									   )
 
 	except Exception as ex:
 		exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -122,7 +128,7 @@ def avvio():
 
 	finally:
 		if tg_bot and "dev" in argv:
-			tg_bot.sendMessage(TELEGRAM_ID,"Ended")
+			tg_bot.sendMessage(TELEGRAM_ID, "Ended")
 		sys.stdout.flush()
 
 
@@ -144,8 +150,8 @@ def dati_statici():
 	global STOP
 	with open(f'{DATASET_CARTELLA_PERCORSO}/{DATASET_NOME_DA_USARE}.csv') as csvFile:
 		datiStatici = csv.reader(csvFile)
-		lastReferenceTime= False
-		frequency = timedelta(minutes = 0)
+		lastReferenceTime = False
+		frequency = timedelta(minutes=0)
 		for riga in datiStatici:
 			# se hai ricevuto il comando di stoppare
 			if STOP:
@@ -156,14 +162,17 @@ def dati_statici():
 					break
 			if riga and riga[0]:
 				tradeTime = datetime.strptime(riga[1], FORMATO_DATA_ORA)
-				if lastReferenceTime is False : #for first-run-only
+				if lastReferenceTime is False:  # for first-run-only
 					lastReferenceTime = tradeTime
 					processaNuovoTrade(float(riga[0]))
 				time_difference = tradeTime - lastReferenceTime
-				pre_time_difference_in_minutes = time_difference / timedelta(minutes=1)
-				time_difference_in_minutes = timedelta(minutes=pre_time_difference_in_minutes)
+				pre_time_difference_in_minutes = time_difference / \
+					timedelta(minutes=1)
+				time_difference_in_minutes = timedelta(
+					minutes=pre_time_difference_in_minutes)
 				if time_difference_in_minutes >= frequency:
-					lastReferenceTime = datetime.strptime(riga[1], FORMATO_DATA_ORA)
+					lastReferenceTime = datetime.strptime(
+						riga[1], FORMATO_DATA_ORA)
 					# gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,"" + str(riga[1]) + " : " + str(riga[0]))
 					processaNuovoTrade(float(riga[0]))
 
@@ -178,26 +187,30 @@ def dati_da_Bitstamp_websocket():
 		now = datetime.now()
 		dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 		cripto, soldi = portafoglio()
-		gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,dt_string+" Sincronizzo bilancio")
+		gestoreRapporti.FileAppend(
+			TRADING_REPORT_FILENAME, dt_string+" Sincronizzo bilancio")
 		balance = json.loads(getBalance())
-		gestoreRapporti.JsonWrites("log/sync_balance.json","w+",balance)
-		cripto_balance = float(balance[f"{VALUTA_CRIPTO}_balance"]) if f"{VALUTA_CRIPTO}_balance" in balance else None
-		soldi_balance = float(balance[f"{VALUTA_SOLDI}_balance"]) if f"{VALUTA_SOLDI}_balance" in balance else None
+		gestoreRapporti.JsonWrites("log/sync_balance.json", "w+", balance)
+		cripto_balance = float(
+			balance[f"{VALUTA_CRIPTO}_balance"]) if f"{VALUTA_CRIPTO}_balance" in balance else None
+		soldi_balance = float(
+			balance[f"{VALUTA_SOLDI}_balance"]) if f"{VALUTA_SOLDI}_balance" in balance else None
 		portafoglio("soldi", soldi_balance)
 		if soldi_balance != soldi:
-			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,dt_string+" Sync balance: "+str(round(soldi_balance-soldi,5))+" "+str(MONETA))
+			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME, dt_string +
+									   " Sync balance: "+str(round(soldi_balance-soldi, 5))+" "+str(MONETA))
 		portafoglio("cripto", cripto_balance)
-		if cripto_balance!=cripto:
-			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,dt_string+" Sync balance: "+str(round(cripto_balance-cripto,8))+" "+str(CRIPTOMONETA))
-
+		if cripto_balance != cripto:
+			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME, dt_string+" Sync balance: "+str(
+				round(cripto_balance-cripto, 8))+" "+str(CRIPTOMONETA))
 
 		# questo mostra piu informazioni se True
 		websocket.enableTrace(False)
 		ws = websocket.WebSocketApp(
-		    "wss://ws.bitstamp.net",
-		    on_message=on_message,
-		    on_error=on_error,
-		    on_close=on_close
+			"wss://ws.bitstamp.net",
+			on_message=on_message,
+			on_error=on_error,
+			on_close=on_close
 		)
 		ws.on_open = on_open
 		ws.run_forever()
@@ -220,10 +233,10 @@ def on_open(_ws):
 		ws {tipo_boh} -- sono dei caratteri apparentemente inutili
 																	"""
 	jsonString = json.dumps({
-	    "event": "bts:subscribe",
-	    "data": {
-	        "channel": f"{CANALE}_{COPPIA_DA_USARE_NOME}"
-	    }
+		"event": "bts:subscribe",
+		"data": {
+			"channel": f"{CANALE}_{COPPIA_DA_USARE_NOME}"
+		}
 	})
 	# manda a bitstamp la richiesta di iscriversi al canale di eventi sopra citato
 	_ws.send(jsonString)
@@ -240,7 +253,6 @@ def on_message(_ws, message: str):
 		processaNuovoTrade(messageDict['data'])
 
 
-
 def on_error(_ws, error: str):
 	global isOpenWS
 	isOpenWS = False
@@ -251,54 +263,61 @@ def on_close(_ws):
 	global isOpenWS
 	isOpenWS = False
 	if tg_bot:
-		tg_bot.sendMessage(TELEGRAM_ID,"WebSocket closed")
+		tg_bot.sendMessage(TELEGRAM_ID, "WebSocket closed")
 	print("### WebSocketclosed ###")
 
 
-
-#______________________________________API______________________________________
+# ______________________________________API______________________________________
 
 # hashing
 def encrypt_string(hash_string):
-    sha_signature = \
-        hashlib.sha256(hash_string.encode()).hexdigest()
-    return sha_signature
+	sha_signature = \
+		hashlib.sha256(hash_string.encode()).hexdigest()
+	return sha_signature
+
 
 @app.route('/ping', methods=['GET'])
 def ping():
 	if 'token' in request.args and encrypt_string(request.args['token']) == API_TOKEN_HASH:
 		return 'online', 200
-	return '',404
+	return '', 404
+
 
 @app.route('/ultimo_valore', methods=['GET'])
 def ultimo_valore():
 	if 'token' in request.args and encrypt_string(request.args['token']) == API_TOKEN_HASH:
 		ultimo_valore = commercialista()[0]
 		return str(ultimo_valore), 200
-	return '',404
+	return '', 404
+
 
 @app.route('/imposta_ultimo_valore', methods=['GET'])
 def imposta_ultimo_valore():
 	if 'token' in request.args and encrypt_string(request.args['token']) == API_TOKEN_HASH:
 		if 'valore' in request.args:
-			ultimo_valore = commercialista("ultimo_valore", float(request.args['valore']))[0]
+			ultimo_valore = commercialista(
+				"ultimo_valore", float(request.args['valore']))[0]
 			return str(ultimo_valore), 200
-	return '',404
+	return '', 404
+
 
 @app.route('/valore_acquisto', methods=['GET'])
 def valore_acquisto():
 	if 'token' in request.args and encrypt_string(request.args['token']) == API_TOKEN_HASH:
 		valore_acquisto = commercialista()[1]
 		return str(valore_acquisto), 200
-	return '',404
+	return '', 404
+
 
 @app.route('/imposta_valore_acquisto', methods=['GET'])
 def imposta_valore_acquisto():
 	if 'token' in request.args and encrypt_string(request.args['token']) == API_TOKEN_HASH:
 		if 'valore' in request.args:
-			valore_acquisto = commercialista("valore_acquisto", float(request.args['valore']))[1]
+			valore_acquisto = commercialista(
+				"valore_acquisto", float(request.args['valore']))[1]
 			return str(valore_acquisto), 200
-	return '',404
+	return '', 404
+
 
 @app.route('/forza_bilancio', methods=['GET'])
 def forza_bilancio():
@@ -307,19 +326,25 @@ def forza_bilancio():
 		now = datetime.now()
 		dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 		cripto, soldi = portafoglio()
-		gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,dt_string+" Sincronizzo bilancio")
+		gestoreRapporti.FileAppend(
+			TRADING_REPORT_FILENAME, dt_string+" Sincronizzo bilancio")
 		balance = json.loads(getBalance())
-		gestoreRapporti.JsonWrites("log/sync_balance.json","w+",balance)
-		cripto_balance = float(balance[f"{VALUTA_CRIPTO}_balance"]) if f"{VALUTA_CRIPTO}_balance" in balance else None
-		soldi_balance = float(balance[f"{VALUTA_SOLDI}_balance"]) if f"{VALUTA_SOLDI}_balance" in balance else None
+		gestoreRapporti.JsonWrites("log/sync_balance.json", "w+", balance)
+		cripto_balance = float(
+			balance[f"{VALUTA_CRIPTO}_balance"]) if f"{VALUTA_CRIPTO}_balance" in balance else None
+		soldi_balance = float(
+			balance[f"{VALUTA_SOLDI}_balance"]) if f"{VALUTA_SOLDI}_balance" in balance else None
 		portafoglio("soldi", soldi_balance)
-		if soldi_balance!=soldi:
-			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,dt_string+" Sync balance: "+str(round(soldi_balance-soldi,5))+" "+str(MONETA))
+		if soldi_balance != soldi:
+			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME, dt_string +
+									   " Sync balance: "+str(round(soldi_balance-soldi, 5))+" "+str(MONETA))
 		portafoglio("cripto", cripto_balance)
-		if cripto_balance!=cripto:
-			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME,dt_string+" Sync balance: "+str(round(cripto_balance-cripto,8))+" "+str(CRIPTOMONETA))
+		if cripto_balance != cripto:
+			gestoreRapporti.FileAppend(TRADING_REPORT_FILENAME, dt_string+" Sync balance: "+str(
+				round(cripto_balance-cripto, 8))+" "+str(CRIPTOMONETA))
 		return str(str(soldi_balance)+" "+MONETA if soldi_balance else str(cripto_balance)+" "+CRIPTOMONETA), 200
-	return '',404
+	return '', 404
+
 
 @app.route('/bilancio', methods=['GET'])
 def bilancio():
@@ -330,7 +355,8 @@ def bilancio():
 		if 'cripto' in request.args:
 			return str(cripto)+" "+CRIPTOMONETA, 200
 		return str(str(soldi)+" "+MONETA if soldi else str(cripto)+" "+CRIPTOMONETA), 200
-	return '',404
+	return '', 404
+
 
 @app.route('/bilancio_stimato', methods=['GET'])
 def bilancio_stimato():
@@ -338,7 +364,8 @@ def bilancio_stimato():
 		cripto, soldi = portafoglio()
 		ultimo_valore = commercialista()[0]
 		return str(str(soldi)+" "+MONETA if soldi else str(cripto*ultimo_valore)+" "+CRIPTOMONETA), 200
-	return '',404
+	return '', 404
+
 
 @app.route('/imposta_bilancio', methods=['GET'])
 def imposta_bilancio():
@@ -347,9 +374,11 @@ def imposta_bilancio():
 			cripto, soldi = portafoglio("soldi", float(request.args['soldi']))
 			return str(soldi)+MONETA, 200
 		if 'cripto' in request.args:
-			cripto, soldi = portafoglio("cripto", float(request.args['cripto']))
+			cripto, soldi = portafoglio(
+				"cripto", float(request.args['cripto']))
 			return str(cripto)+CRIPTOMONETA, 200
-	return '',404
+	return '', 404
+
 
 @app.route('/status', methods=['GET'])
 def status():
@@ -359,7 +388,8 @@ def status():
 		if mybot:
 			return str(mybot.is_alive() or isOpenWS), 200
 		return None, 200
-	return '',404
+	return '', 404
+
 
 @app.route('/stop', methods=['GET'])
 def send_stop():
@@ -369,7 +399,8 @@ def send_stop():
 		# comunica alla strategie di tirare i remi in barca
 		strategiaModulo.closing = True
 		return 'Stopping', 200
-	return '',404
+	return '', 404
+
 
 @app.route('/ask_buy', methods=['GET'])
 def send_buy():
@@ -377,7 +408,8 @@ def send_buy():
 		# comunica alla strategie di comprare
 		strategiaModulo.force_buy = True
 		return 'Buying', 200
-	return '',404
+	return '', 404
+
 
 @app.route('/ask_sell', methods=['GET'])
 def send_sell():
@@ -385,7 +417,8 @@ def send_sell():
 		# comunica alla strategie di comprare
 		strategiaModulo.force_sell = True
 		return 'Selling', 200
-	return '',404
+	return '', 404
+
 
 @app.route('/force_buy', methods=['GET'])
 def force_buy():
@@ -394,7 +427,8 @@ def force_buy():
 		ultimo_valore = commercialista()[0]
 		strategiaModulo.compro(soldi, ultimo_valore)
 		return 'Buying', 200
-	return '',404
+	return '', 404
+
 
 @app.route('/force_sell', methods=['GET'])
 def force_sell():
@@ -403,7 +437,8 @@ def force_sell():
 		ultimo_valore = commercialista()[0]
 		strategiaModulo.vendi(cripto, ultimo_valore)
 		return 'Selling', 200
-	return '',404
+	return '', 404
+
 
 @app.route('/start', methods=['GET'])
 def start_as_daemon():
@@ -415,7 +450,8 @@ def start_as_daemon():
 		mybot = threading.Thread(target=avvio, daemon=True)
 		mybot.start()
 		return 'Started', 200
-	return '',404
+	return '', 404
+
 
 @app.route('/shutdown', methods=['GET'])
 def shutdown():
@@ -426,7 +462,7 @@ def shutdown():
 		func = request.environ.get('werkzeug.server.shutdown')
 		func()
 		return 'API Server going down', 200
-	return '',404
+	return '', 404
 
 
 if __name__ == "__main__":
@@ -437,12 +473,13 @@ if __name__ == "__main__":
 
 		tg_bot = TelegramBot(False)
 
-		app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False, threaded=True)
+		app.run(host='0.0.0.0', port=5000, debug=True,
+				use_reloader=False, threaded=True)
 
 	except Exception as ex:
 		logging.error(ex)
 		if tg_bot:
-			tg_bot.sendMessage(TELEGRAM_ID,"Error, closing")
+			tg_bot.sendMessage(TELEGRAM_ID, "Error, closing")
 	finally:
 		STOP = True
 		strategiaModulo.closing = True
